d301574c1c6c27626a8f7b18112b21173cca2a6a
Vasya <45419685+Gambikus@users.noreply.github.com>
2022-09-14 16:59:57 +0300

    Add first task

---
+++ Домашнее_Задание_1_09.09-16.09/firstTask/src/main/java/components/A.java
@@ -0,0 +1,19 @@
+package components;
+
+public class A {
+    private final int a;
+
+    public A(int a) {
+        this.a = a;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.a;
+    }
+}
---
+++ Домашнее_Задание_1_09.09-16.09/firstTask/src/main/java/components/B.java
@@ -0,0 +1,19 @@
+package components;
+
+public class B {
+    private final int b;
+
+    public B(int b) {
+        this.b = b;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.b;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return false;
+    }
+}

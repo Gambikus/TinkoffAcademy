9302339b5b7eee39827681c329d37bc5d6c4d3db
Vasya Veretin <ver4vas@ya.ru>
2022-09-24 13:52:52 +0300

    change structure of project

--- Домашнее_Задание_1_09.09-16.09/thirdTask/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import components.SingletonExample;
-
-public class Main {
-    public static void main(String[] args) {
-        SingletonExample firstExample = SingletonExample.getInstance();
-        SingletonExample secondExample = SingletonExample.getInstance();
-    }
-}

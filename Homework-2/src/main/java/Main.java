fa8ba96b6e7ee5d7454b376ee0d3dd90407d671e
Vasya Veretin <ver4vas@ya.ru>
2022-09-27 18:57:15 +0300

    fix commit

--- Homework-2/src/main/java/Main.java
+++ Homework-2/src/main/java/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        while (true) {
+            list.add(new ArrayList<Integer>(100000));
+            Thread.sleep(1);
+        }
+    }
 }
